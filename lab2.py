267747692cd1eeaec727aab7abc6f6ae88e418a4
MartynaK <martyna.kindrat@gmail.com>
2024-10-13 13:04:15 +0200

    Lab2 finished

--- lab2.py
+++ lab2.py
@@ -9,7 +9,7 @@ class Dane():
         self.set_points_and_weights()
         
     def set_points_and_weights(self):
-          #dla N=1 , 2D 2 wartosci
+          #2D 2 wartosci
         if self.k==1: #N0
             self.points=[(0,0)]
             self.weights=[4] 
@@ -19,7 +19,16 @@ class Dane():
         elif self.k==3: #N=2
             self.points = [(-math.sqrt(3 / 5), -math.sqrt(3 / 5)),(0, 0),(math.sqrt(3 / 5), math.sqrt(3 / 5))]
             self.weights = [5 / 9, 8 / 9, 5 / 9]
-            #elif self.k==4: i potem N4
+        elif self.k==4: #N3
+            self.points=[(-0.861136,-0.861136),(0.861136,0.861136),(-0.339981,-0.339981),(0.339981,0.339981)]
+            self.weights=[0.347855,0.347855,0.652145,0.652145]
+        elif self.k==5: #N4
+            self.points = [(-0.90617985, -0.90617985),
+                           (-0.53846931, -0.53846931),
+                           (0, 0),
+                           (0.53846931, 0.53846931),
+                           (0.90617985, 0.90617985)]
+            self.weights = [0.23692689, 0.47862867, 0.56888889, 0.47862867, 0.23692689]
             
     def integr_1D(self, func):
         result=0
@@ -40,6 +49,7 @@ def function1D(x):
 def function2D(x,y):
     return 5 * x**2 * y**2 + 3 * x * y + 6
 #1D
+print("Całkowanie :\n")
 dane_1d_2pkt=Dane(2)
 result_1d_2pkt=dane_1d_2pkt.integr_1D(function1D)
 print(f"Wynik calkowania 1d 2pkt :{result_1d_2pkt}")
@@ -47,13 +57,43 @@ dane_1d_3pkt=Dane(3)
 result_1d_3pkt=dane_1d_3pkt.integr_1D(function1D)
 print(f"Wynik calkowania 1d 3pkt :{result_1d_3pkt}")
 
+dane_1d_4pkt = Dane(4)
+result_1d_4pkt = dane_1d_4pkt.integr_1D(function1D)
+print(f"Wynik całkowania 1d 4 pkt: {result_1d_4pkt}")
+
+dane_1d_5pkt = Dane(5)
+result_1d_5pkt = dane_1d_5pkt.integr_1D(function1D)
+print(f"Wynik całkowania 1d 4 pkt: {result_1d_5pkt}\n")
+
 #2D
 dane_2d_2pkt=Dane(2)
 result_2d_2pkt=dane_2d_2pkt.integr_2D(function2D)
 print(f"Wynik calkowania 2d 2pkt :{result_2d_2pkt}")
+
 dane_2d_3pkt=Dane(3)
 result_2d_3pkt=dane_2d_3pkt.integr_2D(function2D)
 print(f"Wynik calkowania 2d 3pkt :{result_2d_3pkt}")
+
+dane_2d_4pkt = Dane(4)
+result_2d_4pkt = dane_2d_4pkt.integr_2D(function2D)
+print(f"Wynik całkowania 2d 4pkt : {result_2d_4pkt}")
+
+dane_2d_5pkt = Dane(5)
+result_2d_5pkt = dane_2d_5pkt.integr_2D(function2D)
+print(f"Wynik całkowania 2d 5pkt : {result_2d_5pkt}")
+
+print("\nMetoda analityczna sprawdzenie: \n")
+
+print(f"Wynik calkowania 1d 2pkt : 16")
+print(f"Wynik calkowania 1d 3pkt : 16")
+print(f"Wynik calkowania 1d 4pkt : 16")
+print(f"Wynik calkowania 1d 5pkt : 16\n")
+
+print(f"Wynik calkowania 2d 2pkt : 25.11")
+print(f"Wynik calkowania 2d 3pkt : 25.11") 
+print(f"Wynik calkowania 2d 4pkt : 25.11") 
+print(f"Wynik calkowania 2d 5pkt : 25.11") 
+
 
 
 
